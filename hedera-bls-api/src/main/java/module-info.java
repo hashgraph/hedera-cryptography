8cc9a956247c86326da55a529618c91002a03acc
Nathan Klick <nathan@swirldslabs.com>
2023-01-26 10:00:10 -0600

    Proposed service loader changes

--- hedera-bls-api/src/main/java/module-info.java
+++ hedera-bls-api/src/main/java/module-info.java
@@ -1,4 +1,7 @@
+import com.hedera.platform.bls.BLS12381BilinearMap;
+
 module com.hedera.platform.bls {
+    // com.hedera.platform.bls12381
     requires resource.loader;
     requires com.sun.jna;
     requires org.apache.logging.log4j;
@@ -6,4 +9,7 @@ module com.hedera.platform.bls {
     requires org.apache.commons.io;
 
     exports com.hedera.platform.bls;
+
+    uses com.hedera.platform.bls.BilinearMap;
+    provides com.hedera.platform.bls.BilinearMap with BLS12381BilinearMap;
 }
