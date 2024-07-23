0c62e851c80a23e3461fb16d765257707667f14e
Roger Barker <roger.barker@swirldslabs.com>
2024-07-23 11:51:32 -0500

    Updated per peer review comments
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,16 +14,4 @@
  * limitations under the License.
  */
 
-/*
-plugins {
-  id("com.hedera.platform.aggregate-reports")
-  id("com.hedera.platform.spotless-conventions")
-  id("com.hedera.platform.spotless-kotlin-conventions")
-}
-
-group = "com.hedera.platform"
-
-repositories { mavenCentral() }
-*/
-
 plugins { id("com.hedera.gradle.root") }
