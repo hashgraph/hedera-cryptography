515948a03be75d11d158fa57210d253c6e9f6204
mxtartaglia <maxi@swirldslabs.com>
2024-07-25 14:18:15 -0300

    Classes for tss public interfaces
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- cryptography/hedera-cryptography-pairings-signatures/src/main/java/module-info.java
+++ cryptography/hedera-cryptography-pairings-signatures/src/main/java/module-info.java
@@ -1,3 +1,5 @@
 module com.hedera.cryptography.pairings.signatures {
     requires transitive com.hedera.cryptography.pairings.api;
+    requires static transitive com.github.spotbugs.annotations;
+    exports com.hedera.cryptography.pairings.signatures.api;
 }
