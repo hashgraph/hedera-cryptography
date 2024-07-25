6b505f5c9e3e338fc1ce54ad4de17e980e2de05a
mxtartaglia <maxi@swirldslabs.com>
2024-07-25 16:25:22 -0300

    Classes for tss public interfaces
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- cryptography/hedera-cryptography-pairings-signatures/src/main/java/module-info.java
+++ cryptography/hedera-cryptography-pairings-signatures/src/main/java/module-info.java
@@ -1,5 +1,9 @@
+/**
+ * This module provides cryptography primitives to create EC PublicKeys, EC PrivateKeys, and Signatures.
+ */
 module com.hedera.cryptography.pairings.signatures {
     requires transitive com.hedera.cryptography.pairings.api;
     requires static transitive com.github.spotbugs.annotations;
+
     exports com.hedera.cryptography.pairings.signatures.api;
 }
