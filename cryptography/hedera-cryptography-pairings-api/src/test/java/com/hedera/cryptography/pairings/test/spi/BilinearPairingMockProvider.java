5592eccace43ed12984075426ca1bb75f9e31acb
mxtartaglia <maxi@swirldslabs.com>
2024-07-31 19:45:08 -0300

    Code review comments
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- cryptography/hedera-cryptography-pairings-api/src/test/java/com/hedera/cryptography/pairings/test/spi/BilinearPairingMockProvider.java
+++ cryptography/hedera-cryptography-pairings-api/src/test/java/com/hedera/cryptography/pairings/test/spi/BilinearPairingMockProvider.java
@@ -388,7 +388,8 @@ public class BilinearPairingMockProvider extends BilinearPairingProvider {
     /**
      * Fake implementation of a {@link BilinearPairing}
      */
-    private record TestBilinearPairing(@NonNull Field field,@NonNull Group group,@NonNull Group group2) implements BilinearPairing {
+    private record TestBilinearPairing(@NonNull Field field, @NonNull Group group, @NonNull Group group2)
+            implements BilinearPairing {
 
         /** {@inheritDoc} */
         @NonNull
