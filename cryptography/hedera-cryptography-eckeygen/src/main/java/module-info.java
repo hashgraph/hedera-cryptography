b3f022ae05b2fd8ddddaa7ffa289085e59a8895e
Lazar Petrovic <lpetrovic05@gmail.com>
2024-10-08 13:03:41 +0200

    remove dependency
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- cryptography/hedera-cryptography-eckeygen/src/main/java/module-info.java
+++ cryptography/hedera-cryptography-eckeygen/src/main/java/module-info.java
@@ -2,7 +2,6 @@
  * This module provides a cryptography utility to create EC PublicKeys and EC PrivateKeys.
  */
 module com.hedera.cryptography.eckeygen {
-    requires com.hedera.common.nativesupport;
     requires com.hedera.cryptography.pairings.api;
     requires com.hedera.cryptography.pairings.signatures;
     requires com.google.protobuf;
