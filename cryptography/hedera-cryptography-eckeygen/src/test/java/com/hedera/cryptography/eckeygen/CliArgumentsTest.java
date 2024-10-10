665385c9124cf888098d3ab744243774aa389c5f
Lazar Petrovic <lpetrovic05@gmail.com>
2024-10-10 15:03:50 +0200

    s
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- cryptography/hedera-cryptography-eckeygen/src/test/java/com/hedera/cryptography/eckeygen/CliArgumentsTest.java
+++ cryptography/hedera-cryptography-eckeygen/src/test/java/com/hedera/cryptography/eckeygen/CliArgumentsTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hedera.cryptography.eckeygen;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,13 +32,13 @@ class CliArgumentsTest {
 
     @Test
     void unknownCommand() {
-        final CliArguments args = CliArguments.parse(new String[]{"some", "random", "args"});
+        final CliArguments args = CliArguments.parse(new String[] {"some", "random", "args"});
         assertEquals(CliCommand.PRINT_HELP, args.command());
     }
 
     @Test
     void insufficientArgs() {
-        final CliArguments args = CliArguments.parse(new String[]{"generate-keys", "private"});
+        final CliArguments args = CliArguments.parse(new String[] {"generate-keys", "private"});
         assertEquals(CliCommand.PRINT_HELP, args.command());
         assertThrows(NullPointerException.class, args::privateKeyPath);
         assertThrows(NullPointerException.class, args::publicKeyPath);
@@ -30,7 +46,7 @@ class CliArgumentsTest {
 
     @Test
     void generateKeysCommand() {
-        final CliArguments args = CliArguments.parse(new String[]{"generate-keys", "private", "public"});
+        final CliArguments args = CliArguments.parse(new String[] {"generate-keys", "private", "public"});
         assertEquals(CliCommand.GENERATE_KEYS, args.command());
         assertNotNull(args.privateKeyPath());
         assertNotNull(args.publicKeyPath());
