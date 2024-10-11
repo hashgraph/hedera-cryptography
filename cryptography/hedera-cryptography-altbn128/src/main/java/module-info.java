73a1d0cbd785e35a5a6b8b9cb71089516c76cec9
Maxi Tartaglia <152629744+mxtartaglia-sl@users.noreply.github.com>
2024-10-11 10:32:34 -0300

    15201: Equals and Hashcode handled by java (#71)
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- cryptography/hedera-cryptography-altbn128/src/main/java/module-info.java
+++ cryptography/hedera-cryptography-altbn128/src/main/java/module-info.java
@@ -4,6 +4,7 @@
 module com.hedera.cryptography.altbn128 {
     requires com.hedera.common.nativesupport;
     requires com.hedera.cryptography.pairings.api;
+    requires com.github.spotbugs.annotations;
 
     provides com.hedera.cryptography.pairings.spi.PairingFriendlyCurveProvider with
             com.hedera.cryptography.altbn128.spi.AltBn128Provider;
