efc6b81d36f568f3ac68933289fd320b3f703402
Lazar Petrovic <lpetrovic05@gmail.com>
2024-10-22 16:18:10 +0200

    s
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- gradle/plugins/src/main/kotlin/com.hedera.gradle.feature.rust.gradle.kts
+++ gradle/plugins/src/main/kotlin/com.hedera.gradle.feature.rust.gradle.kts
@@ -23,7 +23,7 @@ plugins { id("java") }
 
 val cargo = project.extensions.create<CargoExtension>("cargo")
 
-//cargo.targets(aarch64Darwin, aarch64Linux, x86Darwin, x86Linux, x86Windows)
+// cargo.targets(aarch64Darwin, aarch64Linux, x86Darwin, x86Linux, x86Windows)
 // temporarily disable mac compilation because it's failing on GitHub actions
 cargo.targets(aarch64Linux, x86Linux, x86Windows)
 
