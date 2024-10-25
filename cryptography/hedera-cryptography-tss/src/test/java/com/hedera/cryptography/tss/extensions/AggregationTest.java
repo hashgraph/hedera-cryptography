0ed4620f2401fe28899a600bcd583be6913474b0
Maxi Tartaglia <152629744+mxtartaglia-sl@users.noreply.github.com>
2024-10-25 14:48:10 -0300

    feat: 16060 Create Polynomial and FeldmanCommitment (#98)
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

---
+++ cryptography/hedera-cryptography-tss/src/test/java/com/hedera/cryptography/tss/extensions/AggregationTest.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.cryptography.tss.extensions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.hedera.cryptography.bls.BlsKeyPair;
+import com.hedera.cryptography.bls.BlsPrivateKey;
+import com.hedera.cryptography.bls.BlsPublicKey;
+import com.hedera.cryptography.bls.BlsSignature;
+import com.hedera.cryptography.bls.GroupAssignment;
+import com.hedera.cryptography.bls.SignatureSchema;
+import com.hedera.cryptography.pairings.api.Curve;
+import com.hedera.cryptography.pairings.api.FieldElement;
+import com.hedera.cryptography.pairings.api.GroupElement;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+
+public class AggregationTest {
+    private static final Random RANDOM = new SecureRandom();
+
+    @Test
+    void testTargetGroupIsSameAsOriginal() {
+        final long seed = RANDOM.nextLong();
+        System.out.println(seed);
+        var random = new Random(seed);
+        var schema = SignatureSchema.create(Curve.ALT_BN128, GroupAssignment.SHORT_PUBLIC_KEYS);
+        var dealerSecrets = List.of(
+                BlsKeyPair.generate(schema, random),
+                BlsKeyPair.generate(schema, random),
+                BlsKeyPair.generate(schema, random),
+                BlsKeyPair.generate(schema, random));
+        var msg =
+                """
+                        From Wikipedia, the free encyclopedia
+                        This article is about plants in the family Araliaceae. For the typographic ornamentation ❧, see Fleuron (typography). For Hedera Hashgraph, see Hashgraph.
+                        "Ivy" redirects here. For other plants, see list of plants known as ivy. For other uses, see Ivy (disambiguation).
+                        Not to be confused with Hadera.
+                        Hedera, commonly called ivy (plural ivies), is a genus of 12–15 species of evergreen climbing or ground-creeping woody plants in the family Araliaceae, native to Western Europe, Central Europe, Southern Europe, Macaronesia, northwestern Africa and across central-southern Asia east to Japan and Taiwan. Several species are cultivated as climbing ornamentals, and the name ivy especially denotes common ivy (Hedera helix), known in North America as "English ivy", which is frequently planted to clothe brick walls.
+                        """
+                        .getBytes(StandardCharsets.UTF_8);
+
+        final int numberOfParticipants = dealerSecrets.size();
+        final int threshold = dealerSecrets.size();
+        final var dealersIds = IntStream.rangeClosed(1, numberOfParticipants)
+                .boxed()
+                .map(schema.getPairingFriendlyCurve().field()::fromLong)
+                .toList();
+        final var receiverIds = dealersIds;
+        final var signatures = dealerSecrets.stream()
+                .map(p -> p.privateKey().sign(msg))
+                .map(BlsSignature::element)
+                .toList();
+
+        final var publicKeys = dealerSecrets.stream()
+                .map(BlsKeyPair::publicKey)
+                .map(BlsPublicKey::element)
+                .toList();
+        final var aggregatedPk = new BlsPublicKey(Lagrange.recoverGroupElement(dealersIds, publicKeys), schema);
+        final var aggregateSignature = new BlsSignature(Lagrange.recoverGroupElement(dealersIds, signatures), schema);
+        assertTrue(aggregateSignature.verify(aggregatedPk, msg));
+
+        final var aggregatedRekey =
+                secretShareAndRecover(dealerSecrets, random, threshold, threshold, schema, receiverIds, dealersIds);
+
+        assertEquals(aggregatedPk.element(), aggregatedRekey);
+        assertTrue(aggregateSignature.verify(new BlsPublicKey(aggregatedRekey, schema), msg));
+    }
+
+    @Test
+    void testTargetGroupIsDifferentAsOriginal() {
+        final long seed = RANDOM.nextLong();
+        System.out.println(seed);
+        var random = new Random(seed);
+        var schema = SignatureSchema.create(Curve.ALT_BN128, GroupAssignment.SHORT_PUBLIC_KEYS);
+        var dealerSecrets = List.of(
+                BlsKeyPair.generate(schema, random),
+                BlsKeyPair.generate(schema, random),
+                BlsKeyPair.generate(schema, random),
+                BlsKeyPair.generate(schema, random),
+                BlsKeyPair.generate(schema, random),
+                BlsKeyPair.generate(schema, random));
+
+        final int numberOfParticipants = dealerSecrets.size();
+        final int threshold = 12;
+        final var dealersIds = IntStream.rangeClosed(1, numberOfParticipants)
+                .boxed()
+                .map(schema.getPairingFriendlyCurve().field()::fromLong)
+                .toList();
+        final var receiverIds = IntStream.rangeClosed(1, numberOfParticipants * 2)
+                .boxed()
+                .map(schema.getPairingFriendlyCurve().field()::fromLong)
+                .toList();
+
+        final var publicKeys = dealerSecrets.stream()
+                .map(BlsKeyPair::publicKey)
+                .map(BlsPublicKey::element)
+                .toList();
+        final var aggregatedPk = new BlsPublicKey(Lagrange.recoverGroupElement(dealersIds, publicKeys), schema);
+
+        final var aggregatedRekey = secretShareAndRecover(
+                dealerSecrets, random, dealersIds.size(), threshold, schema, receiverIds, dealersIds);
+
+        assertEquals(aggregatedPk.element(), aggregatedRekey);
+    }
+
+    private GroupElement secretShareAndRecover(
+            final List<BlsKeyPair> dealerSecrets,
+            final Random random,
+            final int previousThreshold,
+            final int currentThreshold,
+            final SignatureSchema schema,
+            final List<FieldElement> receiverIds,
+            final List<FieldElement> dealersIds) {
+
+        final var selectedDealers = dealersIds.stream().limit(previousThreshold).toList();
+        final var privateKeys = dealerSecrets.stream()
+                .limit(previousThreshold)
+                .map(BlsKeyPair::privateKey)
+                .map(BlsPrivateKey::element)
+                .toList();
+        final var polynomials = privateKeys.stream()
+                .map(s -> Polynomial.fromValue(random, s, currentThreshold - 1))
+                .limit(previousThreshold)
+                .toList();
+        final var polynomialsCommitments = polynomials.stream()
+                .map(poly -> FeldmanCommitment.create(schema.getPublicKeyGroup(), poly))
+                .toList();
+        final var polynomialPrivatesPoints = polynomials.stream()
+                .map(poly -> receiverIds.stream()
+                        .limit(currentThreshold)
+                        .map(poly::evaluate)
+                        .toList())
+                .toList();
+        final var polynomialPublications = polynomialsCommitments.stream()
+                .map(poly -> receiverIds.stream()
+                        .limit(currentThreshold)
+                        .map(poly::evaluate)
+                        .toList())
+                .toList();
+
+        final var ssPoints = this.reArrange(currentThreshold, polynomialPrivatesPoints);
+        final var psPoints = this.reArrange(currentThreshold, polynomialPublications);
+
+        final var rekeyPublicShares = psPoints.stream()
+                .map(l -> Lagrange.recoverGroupElement(selectedDealers, l))
+                .toList();
+        final var rekeySecretShares = ssPoints.stream()
+                .map(l -> Lagrange.recoverFieldElement(selectedDealers, l))
+                .toList();
+
+        zipStream(rekeySecretShares, rekeyPublicShares)
+                .forEach(entry -> assertEquals(
+                        entry.getValue().getGroup().generator().multiply(entry.getKey()), entry.getValue()));
+
+        return Lagrange.recoverGroupElement(receiverIds, rekeyPublicShares);
+    }
+
+    public static <A, B> Stream<Entry<A, B>> zipStream(final List<A> aCollection, final List<B> bCollection) {
+        return IntStream.range(0, Math.min(aCollection.size(), bCollection.size()))
+                .boxed()
+                .map(i -> Map.entry(aCollection.get(i), bCollection.get(i)));
+    }
+
+    private <T> List<List<T>> reArrange(int maxSize, final List<List<T>> polynomialPrivatesPoints) {
+        return IntStream.range(0, polynomialPrivatesPoints.getFirst().size())
+                .boxed()
+                .map(i -> polynomialPrivatesPoints.stream()
+                        .map(points -> points.get(i))
+                        .toList())
+                .limit(maxSize)
+                .toList();
+    }
+}
