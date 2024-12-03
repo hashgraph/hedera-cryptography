0d398f7f4bc8c3c50b6425f857d8aeb40686fa0d
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-12-03 15:42:45 +0100

    tmp: disable caching of 'installRustToolchains' on CI
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- gradle/plugins/src/main/kotlin/com.hedera.gradle.feature.rust.gradle.kts
+++ gradle/plugins/src/main/kotlin/com.hedera.gradle.feature.rust.gradle.kts
@@ -22,3 +22,8 @@ plugins { id("java") }
 val cargo = project.extensions.create<CargoExtension>("cargo")
 
 cargo.targets(*CargoToolchain.values())
+
+if (System.getenv().containsKey("CI")) {
+    // Disable, due to 'Could not load entry ... from remote build cache: Read timed out'
+    tasks.named("installRustToolchains") { outputs.cacheIf { false } }
+}
