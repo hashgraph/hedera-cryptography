71badc38377903e058bc83408bfb080edd704d93
mxtartaglia <maxi@swirldslabs.com>
2024-12-04 15:20:14 -0300

    feat(16306): complete bls tests
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- cryptography/hedera-cryptography-bls/src/testFixtures/java/module-info.java
+++ cryptography/hedera-cryptography-bls/src/testFixtures/java/module-info.java
@@ -2,7 +2,5 @@ module com.hedera.cryptography.bls.test.fixtures {
     requires com.hedera.cryptography.utils.test.fixtures;
     requires static transitive com.hedera.cryptography.bls;
 
-    uses com.hedera.cryptography.pairings.api.PairingFriendlyCurve;
-
     exports com.hedera.cryptography.bls.test.fixtures;
 }
