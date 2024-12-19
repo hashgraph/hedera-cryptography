1d81d77610898a337598d9c77d0ee5fcf8975aaf
Lazar Petrovic <lpetrovic05@gmail.com>
2024-12-19 12:34:13 +0100

    fix compile
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- cryptography/hedera-cryptography-pairings-api/src/test/java/module-info.java
+++ cryptography/hedera-cryptography-pairings-api/src/test/java/module-info.java
@@ -3,6 +3,7 @@ open module com.hedera.cryptography.pairings.test {
     requires com.hedera.cryptography.pairings.test.fixtures;
     requires com.hedera.cryptography.utils.test.fixtures;
     requires org.junit.jupiter.api;
+    requires org.junit.jupiter.params;
 
     uses com.hedera.cryptography.pairings.api.PairingFriendlyCurve;
 }
