81e834bd723add348e1cc35e8048f0bce2ad13fb
anthony-swirldslabs <152534762+anthony-swirldslabs@users.noreply.github.com>
2025-01-16 10:15:43 -0800

    feat: introduce HintsLibraryBridge (#227)
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- cryptography/hedera-cryptography-hinTS/src/main/java/module-info.java
+++ cryptography/hedera-cryptography-hinTS/src/main/java/module-info.java
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
 /**
  */
-module com.hedera.cryptography.hints {}
+module com.hedera.cryptography.hints {
+    exports com.hedera.cryptography.hints;
+
+    requires com.hedera.common.nativesupport;
+}
