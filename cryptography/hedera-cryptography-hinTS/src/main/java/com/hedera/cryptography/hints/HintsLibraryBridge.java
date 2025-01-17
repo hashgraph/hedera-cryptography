a64d98068cfa80999fd8f579cf362ef501088d02
anthony-swirldslabs <152534762+anthony-swirldslabs@users.noreply.github.com>
2025-01-17 12:27:02 -0800

    feat: support CRS in HintsLibraryBridge (#232)
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- cryptography/hedera-cryptography-hinTS/src/main/java/com/hedera/cryptography/hints/HintsLibraryBridge.java
+++ cryptography/hedera-cryptography-hinTS/src/main/java/com/hedera/cryptography/hints/HintsLibraryBridge.java
@@ -34,6 +34,38 @@ public class HintsLibraryBridge {
         return INSTANCE_HOLDER.getInstance();
     }
 
+    ///////////////////////////////////////////////////////////////////////////
+    //                    CRS management methods
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Generates a new CRS object for the given number of signers.
+     * @param signersNum the number of signers
+     * @return the CRS object
+     */
+    public native byte[] initCRS(long signersNum);
+
+    /**
+     * Update a previous CRS object with a new contribution.
+     * @param prevCRS the previous CRS object
+     * @param random the random contribution
+     * @return the updated CRS object and a concatenated contribution proof for the update
+     */
+    public native byte[] updateCRS(final byte[] prevCRS, final byte[] random);
+
+    /**
+     * Verifies an updated CRS object.
+     * @param prevCRS the previous CRS object
+     * @param nextCRS the updated CRS object
+     * @param contributionProof the contribution proof
+     * @return true if the updated CRS object is valid, false otherwise
+     */
+    public native boolean verifyCRS(final byte[] prevCRS, final byte[] nextCRS, final byte[] contributionProof);
+
+    ///////////////////////////////////////////////////////////////////////////
+    //                          HinTS APIs
+    ///////////////////////////////////////////////////////////////////////////
+
     /**
      * Generates a new BLS key pair.
      * @return the key pair
@@ -43,22 +75,24 @@ public class HintsLibraryBridge {
     /**
      * Computes the hints for the given public key and number of parties.
      *
+     * @param crs the CRS object
      * @param blsPrivateKey the private key
      * @param partyId the party id
      * @param n the number of parties
      * @return the hints
      */
-    public native byte[] computeHints(final byte[] blsPrivateKey, int partyId, int n);
+    public native byte[] computeHints(final byte[] crs, final byte[] blsPrivateKey, int partyId, int n);
 
     /**
      * Validates the hinTS public key for the given number of parties.
      *
-     * @param hintsKey the hinTS key
+     * @param crs the CRS object
+     * @param hintsPublicKey the hinTS key
      * @param partyId the party id
      * @param n the number of parties
      * @return true if the hints are valid; false otherwise
      */
-    public native boolean validateHintsKey(final byte[] hintsKey, int partyId, int n);
+    public native boolean validateHintsKey(final byte[] crs, final byte[] hintsPublicKey, int partyId, int n);
 
     /**
      * Runs the hinTS preprocessing algorithm on the given validated hint keys and party weights for the given number
@@ -68,15 +102,17 @@ public class HintsLibraryBridge {
      *     well-formed aggregate public key.</li>
      *     <li>The succinct verification key to use when verifying an aggregate signature.</li>
      * </ol>
-     * The parties, hintsKeys, and weights model the original {@code Map<Integer, Bytes> hintsKeys} and {@code
+     * The parties, hintsPublicKeys, and weights model the original {@code Map<Integer, Bytes> hintsPublicKeys} and {@code
      * Map<Integer, Long> weights} input arguments and use arrays for performance reasons.
-     * @param parties the party ids for the indices in hintsKeys and weights
-     * @param hintsKeys the valid hinTS keys by party id
+     * @param crs the CRS object
+     * @param parties the party ids for the indices in hintsPublicKeys and weights
+     * @param hintsPublicKeys the valid hinTS keys by party id
      * @param weights the weights by party id
      * @param n the number of parties
      * @return the preprocessed keys
      */
-    public native byte[] preprocess(final int[] parties, final byte[][] hintsKeys, final long[] weights, int n);
+    public native byte[] preprocess(
+            final byte[] crs, final int[] parties, final byte[][] hintsPublicKeys, final long[] weights, int n);
 
     /**
      * Signs a message with a BLS private key.
@@ -90,16 +126,19 @@ public class HintsLibraryBridge {
     /**
      * Checks that a signature on a message verifies under a BLS public key.
      *
+     * @param crs the CRS object
      * @param signature the signature
      * @param message the message
      * @param publicKey the public key
      * @return true if the signature is valid; false otherwise
      */
-    public native boolean verifyBls(final byte[] signature, final byte[] message, final byte[] publicKey);
+    public native boolean verifyBls(
+            final byte[] crs, final byte[] signature, final byte[] message, final byte[] publicKey);
 
     /**
      * Aggregates the signatures for party ids using hinTS aggregation and verification keys.
      *
+     * @param crs the CRS object
      * @param aggregationKey the aggregation key
      * @param verificationKey the verification key
      * @param parties the party ids for the partialSignatures array
@@ -107,13 +146,18 @@ public class HintsLibraryBridge {
      * @return the aggregated signature
      */
     public native byte[] aggregateSignatures(
-            byte[] aggregationKey, byte[] verificationKey, int[] parties, byte[][] partialSignatures);
+            final byte[] crs,
+            final byte[] aggregationKey,
+            final byte[] verificationKey,
+            final int[] parties,
+            final byte[][] partialSignatures);
 
     /**
      * Checks an aggregate signature on a message verifies under a hinTS verification key, where
      * this is only true if the aggregate signature has weight exceeding the specified threshold
      * or total weight stipulated in the verification key.
      *
+     * @param crs the CRS object
      * @param signature the aggregate signature
      * @param message the message
      * @param verificationKey the verification key
@@ -122,9 +166,10 @@ public class HintsLibraryBridge {
      * @return true if the signature is valid; false otherwise
      */
     public native boolean verifyAggregate(
-            byte[] signature,
-            byte[] message,
-            byte[] verificationKey,
+            final byte[] crs,
+            final byte[] signature,
+            final byte[] message,
+            final byte[] verificationKey,
             long thresholdNumerator,
             long thresholdDenominator);
 }
