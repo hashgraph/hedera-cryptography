401561d7da35ee91e13e41f42d7211f01619728b
Anthony Petrov <anthony@swirldslabs.com>
2025-01-22 10:57:19 -0800

    feat: implement CRS support JNI methods
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- cryptography/hedera-cryptography-hinTS/build.gradle.kts
+++ cryptography/hedera-cryptography-hinTS/build.gradle.kts
@@ -2,8 +2,9 @@
 plugins {
     id("org.hiero.gradle.module.library")
     id("org.hiero.gradle.feature.rust")
-    id("org.hiero.gradle.feature.test-fixtures")
     id("org.hiero.gradle.feature.test-multios")
 }
 
 cargo { libname = "hints" }
+
+testModuleInfo { requires("org.junit.jupiter.api") }
