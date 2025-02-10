b647afd3eab2e892dcb07820f719d661d1620dae
anthony-swirldslabs <152534762+anthony-swirldslabs@users.noreply.github.com>
2025-02-10 13:48:51 -0800

    feat: introduce tests for HistoryLibraryBridge (#259)
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- cryptography/hedera-cryptography-rpm/build.gradle.kts
+++ cryptography/hedera-cryptography-rpm/build.gradle.kts
@@ -2,8 +2,12 @@
 plugins {
     id("org.hiero.gradle.module.library")
     id("org.hiero.gradle.feature.rust")
-    id("org.hiero.gradle.feature.test-fixtures")
     id("org.hiero.gradle.feature.test-multios")
 }
 
 cargo { libname = "rpm" }
+
+testModuleInfo {
+    requires("org.junit.jupiter.api")
+    requires("com.hedera.cryptography.hints")
+}
