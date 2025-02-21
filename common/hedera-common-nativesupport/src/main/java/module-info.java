a56d4b016f340ce2b7c7d2e569d2541c228eda5f
Anthony Petrov <anthony@swirldslabs.com>
2025-02-21 15:45:31 -0800

    chore: remove spotbugs
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- common/hedera-common-nativesupport/src/main/java/module-info.java
+++ common/hedera-common-nativesupport/src/main/java/module-info.java
@@ -1,9 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
 /**
  * Provides a set of generic functions loading native libraries in different system architectures when packaged
  * in a jar using a predefined organization, so they can be accessed with JNI.
  */
 module com.hedera.common.nativesupport {
     exports com.hedera.common.nativesupport;
-
-    requires static transitive com.github.spotbugs.annotations;
 }
