ed7347e89dbe016cda1712b8c18e7b8bd8fb9b86
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-04-17 12:40:24 +0200

    build: bump org.hiero.gradle.build to 0.3.10
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- cryptography/hedera-cryptography-hinTS/build.gradle.kts
+++ cryptography/hedera-cryptography-hinTS/build.gradle.kts
@@ -1,8 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
-import java.nio.file.Files
-import java.nio.file.StandardCopyOption
-import org.hiero.gradle.tasks.CargoBuildTask
-
 plugins {
     id("org.hiero.gradle.module.library")
     id("org.hiero.gradle.feature.rust")
@@ -14,24 +10,4 @@ cargo { libname = "hints" }
 testModuleInfo {
     requires("org.junit.jupiter.api")
     requires("org.junit.jupiter.params")
-}
-
-// TODO jjohannes: once finalized, move this into Gradle plugins
-tasks.withType<CargoBuildTask> {
-    doLast {
-        val originalPath = toolchain.get().folder
-        val adjustedPath = originalPath.replace("software/", "com/hedera/nativelib/hints/")
-        val originalFile =
-            destinationDirectory.dir(originalPath).get().asFile.listFiles()!!.single().toPath()
-        val adjustedFile =
-            destinationDirectory
-                .dir(adjustedPath)
-                .get()
-                .asFile
-                .toPath()
-                .resolve(originalFile.fileName)
-        Files.createDirectories(adjustedFile.parent)
-        Files.move(originalFile, adjustedFile, StandardCopyOption.REPLACE_EXISTING)
-        destinationDirectory.dir("software").get().asFile.delete()
-    }
 }
--- cryptography/hedera-cryptography-rpm/build.gradle.kts
+++ cryptography/hedera-cryptography-rpm/build.gradle.kts
@@ -1,8 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
-import java.nio.file.Files
-import java.nio.file.StandardCopyOption
-import org.hiero.gradle.tasks.CargoBuildTask
-
 plugins {
     id("org.hiero.gradle.module.library")
     id("org.hiero.gradle.feature.rust")
@@ -18,23 +14,3 @@ testModuleInfo {
 }
 
 tasks.test { environment(mapOf("TSS_LIB_NUM_OF_CORES" to "10")) }
-
-// TODO jjohannes: once finalized, move this into Gradle plugins
-tasks.withType<CargoBuildTask> {
-    doLast {
-        val originalPath = toolchain.get().folder
-        val adjustedPath = originalPath.replace("software/", "com/hedera/nativelib/raps/")
-        val originalFile =
-            destinationDirectory.dir(originalPath).get().asFile.listFiles()!!.single().toPath()
-        val adjustedFile =
-            destinationDirectory
-                .dir(adjustedPath)
-                .get()
-                .asFile
-                .toPath()
-                .resolve(originalFile.fileName)
-        Files.createDirectories(adjustedFile.parent)
-        Files.move(originalFile, adjustedFile, StandardCopyOption.REPLACE_EXISTING)
-        destinationDirectory.dir("software").get().asFile.delete()
-    }
-}
