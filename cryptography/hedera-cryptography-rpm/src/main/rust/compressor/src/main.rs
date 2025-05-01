1415a87e79c17e89e98dbb5aa1965a72429b4633
Rohit Sinha <rohit@hashgraph.com>
2025-05-01 15:21:25 -0700

    adding 4 bytes of length; also avoiding linking with RAPS lib
    Signed-off-by: Rohit Sinha <rohit@hashgraph.com>

--- cryptography/hedera-cryptography-rpm/src/main/rust/compressor/src/main.rs
+++ cryptography/hedera-cryptography-rpm/src/main/rust/compressor/src/main.rs
@@ -1,7 +1,15 @@
-use raps::raps::RAPS as RAPS;
-use sp1_sdk::{SP1ProofWithPublicValues, SP1ProvingKey, SP1VerifyingKey};
+use sp1_sdk::{
+    SP1ProofWithPublicValues,
+    SP1ProvingKey,
+    SP1VerifyingKey,
+    ProverClient,
+    SP1Stdin,
+    HashableKey
+};
 use std::io::{self, Read, Write};
 use byteorder::{BigEndian, ReadBytesExt};
+use alloy_sol_types::SolType;
+use ab_rotation_lib::{statement::CompressedStatement, PublicValuesStruct, errors::*};
 
 // this method is used to read a byte array from a reader
 // it first reads a 4-byte length prefix, which is in big endian
@@ -13,6 +21,45 @@ fn read_byte_array<R: Read>(reader: &mut R) -> io::Result<Vec<u8>> {
     let mut buf = vec![0u8; len as usize];
     reader.read_exact(&mut buf)?;
     Ok(buf)
+}
+
+fn compress_rotation_proof(
+    compression_pk: &SP1ProvingKey,               // proving key output by sp1 setup for compression zkVM
+    raps_vk: &SP1VerifyingKey,                    // verifying key output by sp1 setup for RAPS zkVM
+    proof: SP1ProofWithPublicValues,              // the proof to compress
+) -> Result<SP1ProofWithPublicValues, RAPSError>{
+    let prover = ProverClient::builder().cpu().build();
+
+    let parsed_proof = PublicValuesStruct::abi_decode(&proof.public_values.to_vec(), true)
+        .map_err(|_| RAPSError::InvalidInput(("error decoding previous proof").to_string()))?;
+
+    let statement = CompressedStatement {
+        vk_digest: raps_vk.hash_u32(),
+        ab_genesis_hash: parsed_proof.ab_genesis_hash.0,
+        ab_current_hash: parsed_proof.ab_curr_hash.0,
+        ab_next_hash: parsed_proof.ab_next_hash.0,
+        tss_vk_current_hash: parsed_proof.tss_vk_hash.0,
+    };
+
+    // Supply the statement and (optional) prev proof to the zkVM
+    let mut stdin = SP1Stdin::new();
+    stdin.write(&statement);
+
+    let box_proof_inner = proof
+        .proof
+        .try_as_compressed()
+        .ok_or(RAPSError::InvalidInput("expected valid proof to compress".to_string()))?;
+
+    stdin.write_proof(*box_proof_inner, raps_vk.vk.clone());
+
+    // Generate the proofs
+    let compressed_proof: SP1ProofWithPublicValues = prover
+        .prove(compression_pk, &stdin)
+        .groth16()
+        .run()
+        .map_err(|_| RAPSError::ProverError)?;
+
+    Ok(compressed_proof)
 }
 
 fn main() -> io::Result<()> {
@@ -30,7 +77,7 @@ fn main() -> io::Result<()> {
     let proof: SP1ProofWithPublicValues = bincode::deserialize(proof.as_ref()).expect("failed to deserialize prev_proof");
 
     // the main compression logic
-    let compressed_proof = RAPS::compress_rotation_proof(
+    let compressed_proof = compress_rotation_proof(
         &compression_pk,
         &raps_vk,
         proof,
@@ -42,6 +89,10 @@ fn main() -> io::Result<()> {
 
     let stdout = io::stdout();
     let mut handle = stdout.lock();
+
+    // let us first write the length of the compressed proof
+    let len_bytes = (compressed_proof_buf.len() as u32).to_be_bytes();
+    handle.write_all(&len_bytes)?;
 
     handle.write_all(&compressed_proof_buf)?; // Writes the bytes
     handle.flush()?;          // Ensures the data is written immediately
