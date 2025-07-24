835faf48cc79bf0172524108405d0378945d5369
Anthony Petrov <anthony@swirldslabs.com>
2025-07-24 11:09:07 -0700

    do use clang, not clang-cl
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- cryptography/hedera-cryptography-rpm/build.gradle.kts
+++ cryptography/hedera-cryptography-rpm/build.gradle.kts
@@ -204,7 +204,7 @@ tasks.withType<CargoBuildTask> {
 
                     // processBuilder.environment().put("CC_FOR_TARGET", "gcc-aarch64-linux-gnu")
                     //processBuilder.environment().put("CC", "aarch64-linux-gnu-gcc")
-                    processBuilder.environment().put("CC", "clang-cl")
+                    processBuilder.environment().put("CC", "clang")
                     // processBuilder.environment().put("CC_FOR_TARGET", "aarch64-linux-gnu-gcc")
                     // processBuilder.environment().put("CXX", "aarch64-linux-gnu-g++")
                     // processBuilder.environment().put("CXX_FOR_TARGET", "aarch64-linux-gnu-g++")
