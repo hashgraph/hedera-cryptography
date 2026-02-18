b16cab7b18e4e392cca9d621b748b3bae403fa02
Jendrik Johannes <jendrik@onepiece.software>
2026-02-18 22:31:29 +0100

    build: downgrade nmcp
    The update likely broke some SNAPSHOT publishing setups
    
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ pluginManagement { includeBuild("gradle/plugins") }
 
 plugins { id("org.hiero.gradle.build") version "0.6.3" }
 
+buildscript { dependencies.constraints { classpath("com.gradleup.nmcp:nmcp:1.2.1!!") } }
+
 rootProject.name = "hedera-cryptography"
 
 javaModules {
