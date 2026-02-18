c5ba5192b62818ec4ba619ed4c059da35c1bdb6e
Jendrik Johannes <jendrik@onepiece.software>
2026-02-18 12:29:32 +0100

    build: update org.hiero.gradle.build to 0.7.1 (final)
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://central.sonatype.com/repository/maven-snapshots")
-    }
-    includeBuild("gradle/plugins")
-}
+pluginManagement { includeBuild("gradle/plugins") }
 
-buildscript {
-    configurations.classpath { resolutionStrategy.cacheChangingModulesFor(0, "seconds") }
-}
-
-plugins { id("org.hiero.gradle.build") version "0.7.1-SNAPSHOT" }
+plugins { id("org.hiero.gradle.build") version "0.7.1" }
 
 rootProject.name = "hedera-cryptography"
 
